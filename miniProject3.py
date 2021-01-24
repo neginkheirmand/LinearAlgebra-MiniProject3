d7300674be916055341a55749857433048f16557
neginkheirmand <neginkheirmand@gmail.com>
2021-01-24 23:41:36 +0330

    added linear regression method

--- miniProject3.py
+++ miniProject3.py
@@ -12,7 +12,31 @@ def readData(nameFile):
         print("Can't find specified file") 
 
 
-def linearRegression(A, b):
+def linearRegression(data):
+    #since its linear regression the A matrix will be something like:
+    #       | 1   x1|
+    # A=    | 1   x2|
+    #       | 1   x3|
+    # and the b matrix would be:
+    #       |   y1  |
+    #  b=   |   y2  |
+    #       |   y3  |
+    A = np.ones((len(data), 2))
+    b = np.ones((len(data)))
+    # data contains pairs of x and y : data =[ ... [xn, yn], [x(n+1), y(n+1)]   ... ]
+    for i in range(0, len(data)):
+        x = data[i][0]
+        y = data[i][1]
+        A[i][1] = x
+        b[i] = y
+    print("Linear Regression")
+    print(A)
+    print(b)
+    newX = Regression(A, b)
+
+
+
+def Regression(A, b):
     #this function solves the least square
     # for the equation Ax=b
     # by using the AᵗA xˆ= Aᵗ b
@@ -25,11 +49,11 @@ def linearRegression(A, b):
     #now find the newx for which the equation newA * newx = newb
     #since we are sure the newA is not singular and invertible 
     newx = np.linalg.solve(newA, newb)
-    for i in range(0, len(newx)):
-        print(newx[i])
+    
 
 if __name__ == "__main__":
     # readData("GOOGL.csv")
-    A = np.array([ [1,2], [1,5], [1,7], [1,8] ])
-    b = np.array([ 1, 2, 3, 3])
-    linearRegression(A, b)
+    # A = np.array([ [1,1,0,0], [1,1,0,0], [1,0,1,0], [1,0,1,0], [1,0,0,1], [1,0,0,1]])
+    # b = np.array([ -3, -1, 0, 2, 5, 1])
+    # Regression(A, b)
+    linearRegression([[1,2], [2,3], [3, 4]])
